3313bf0d287629ef73d353d065e037ba502147e3
Quanghuy128 <quanghuyhihi128@gmail.com>
2023-03-29 14:46:30 +0700

    007.2.using_generic_validation_pipeline_validation

---
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,46 @@
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+        where TResponse : IErrorOr
+    {
+        private readonly IValidator<TRequest>? _validator;
+
+        public ValidationBehavior(IValidator<TRequest>? validator = null)
+        {
+            _validator = validator;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request, 
+            RequestHandlerDelegate<TResponse> next, 
+            CancellationToken cancellationToken)
+        {
+            if(_validator is null)
+            {
+                return await next();
+            }
+            var validationResult = await _validator?.ValidateAsync(request, cancellationToken);
+            if (validationResult.IsValid)
+            {
+                return await next();
+            }
+
+            var errors = validationResult.Errors
+                .Select(validationFailure => Error.Validation
+                (
+                    validationFailure.PropertyName, 
+                    validationFailure.ErrorMessage)
+                )
+                .ToArray();
+            return (dynamic) errors;
+        }
+    }
+}
