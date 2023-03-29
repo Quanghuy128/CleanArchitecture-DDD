1d976bc81438a479984e5e8913d1d65e0650b5f0
Quanghuy128 <quanghuyhihi128@gmail.com>
2023-03-29 23:16:30 +0700

    009.1.domain_layer_struture&skeleton__1

---
+++ BuberDinner.Domain/Menu/Menu.cs
@@ -0,0 +1,16 @@
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Menu.Entities;
+using BuberDinner.Domain.Menu.ValueObjects;
+
+namespace BuberDinner.Domain.Menu
+{
+    public sealed class Menu : AggregateRoot<MenuId>
+    {
+        private readonly List<MenuSection> _sections = new();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float AverageRating { get; set; }
+
+        public IReadOnlyList<MenuSection> Sections => _sections.AsReadOnly();
+    }
+}
---
+++ BuberDinner.Domain/Menu/ValueObjects/MenuId.cs
@@ -0,0 +1,22 @@
+﻿using BuberDinner.Domain.Common.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BuberDinner.Domain.Menu.ValueObjects
+{
+    public sealed class MenuId : ValueObject
+    {
+        public Guid Value { get; }
+        private MenuId(Guid value) {
+            Value = value;
+        }
+
+        public static MenuId CreateUnique()
+        {
+            return new(Guid.NewGuid());
+        }
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
