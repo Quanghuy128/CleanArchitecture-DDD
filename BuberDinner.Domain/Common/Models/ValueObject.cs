2777da1cbc1f7426a7056ab54657e4c1b48223c4
Quanghuy128 <quanghuyhihi128@gmail.com>
2023-03-29 16:54:26 +0700

    008.1.implement_value_object

---
+++ BuberDinner.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Domain.Common.Models
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null || obj.GetType() != typeof(ValueObject))
+            {
+                return false;
+            }
+            var valueObject = obj as ValueObject;
+            return valueObject.GetEqualityComponents().
+                SequenceEqual(valueObject.GetEqualityComponents());
+        }
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?) other);
+        }
+    }
+    public class Price : ValueObject
+    {
+        public decimal Amount { get; private set; }
+        public decimal Currency { get; private set; }
+
+        public Price(decimal amount, decimal currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Amount;
+            yield return Currency;
+        }
+    }
+}
